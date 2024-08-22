c1534df5833eef3be6d16dab67e9e8b0e9ce1045
Nastya3264 <nastenka.nastenka@bk.ru>
2024-08-23 00:46:15 +0300

    Задача 2

--- Reading files in python.py
+++ Reading files in python.py
@@ -39,3 +39,36 @@ answer += letter * int(number)
 
 with open('dataset_3363_2.txt', 'a') as file:
   file.write(answer)
+
+# Задача 2
+# Недавно мы считали для каждого слова количество его вхождений в строку.
+# Но на все слова может быть не так интересно смотреть, как, например, на наиболее часто используемые.
+# Напишите программу, которая считывает текст из файла (в файле может быть больше одной строки)
+# и выводит самое частое слово в этом тексте и через пробел то, сколько раз оно встретилось. Если таких слов несколько,
+# вывести лексикографически первое (можно использовать оператор < для строк).
+# В качестве ответа укажите вывод программы, а не саму программу.
+# Слова, написанные в разных регистрах, считаются одинаковыми.
+
+d = {}
+
+with open('dataset_3363_3.txt') as file:
+    for line in file:
+        line = line.lower()
+        for word in line.split():
+            if word not in d:
+                d[word] = 1
+            else:
+                d[word] += 1
+
+max_quantity = 1
+# переменная счетчик с макс.кол-вом
+
+for key, value in d.items():
+    current_quantity = d[key]
+    if current_quantity > max_quantity:
+        max_quantity = current_quantity
+        word_with_max_quantity = key
+
+with open('dataset_3363_3.txt', 'a') as file:
+    most_popular = (word_with_max_quantity + ' ' + str(max_quantity))
+    file.write(most_popular)
