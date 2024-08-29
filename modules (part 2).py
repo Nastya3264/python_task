82cfa1844a73d168d0e48b4027032bb48b4c4d9a
Nastya3264 <139234245+Nastya3264@users.noreply.github.com>
2024-08-30 01:02:28 +0300

    Task 2

--- modules (part 2).py
+++ modules (part 2).py
@@ -8,23 +8,35 @@
 # В поле ответа введите одно число или отправьте файл, содержащий одно число.
 
 # (1 способ)
-import requests
-url = 'https://stepik.org/media/attachments/course67/3.6.2/662.txt'
-r = requests.get(url)
-text_file = r.text.splitlines()
-print(text_file)
+# import requests
+# url = 'https://stepik.org/media/attachments/course67/3.6.2/662.txt'
+# r = requests.get(url)
+# text_file = r.text.splitlines()
+# print(text_file)
+#
+# count = 0
+# for i in text_file:
+#         count += 1
+# print(count)
+#
+# # (2 способ)
+# from requests import get
+# with open('dataset_3378_2.txt') as file:
+#         url = file.readline().strip()
+# text = get(url).text.splitlines()
+# print(text)
+# print(len(text))
 
-count = 0
-for i in text_file:
-        count += 1
-print(count)
+# Задача 2
+# Имеется набор файлов, каждый из которых, кроме последнего, содержит имя следующего файла.
+# Первое слово в тексте последнего файла: "We".
+# Скачайте предложенный файл. В нём содержится ссылка на первый файл из этого набора.
+# Все файлы располагаются в каталоге по адресу:
+# https://stepik.org/media/attachments/course67/3.6.3/
+# Загрузите содержимое последнего файла из набора, как ответ на это задание.
 
-# (2 способ)
 from requests import get
-with open('dataset_3378_2.txt') as file:
-        url = file.readline().strip()
-text = get(url).text.splitlines()
-print(text)
-print(len(text))
-
-
+file = get('https://stepik.org/media/attachments/course67/3.6.3/699991.txt')
+while not file.text.startswith('We'):
+        file = get('https://stepik.org/media/attachments/course67/3.6.3/' + file.text.strip())
+print(file.text)
